9549fcceff8efd9d92289bf85e2b385750495ccc
gitgut0110 <jsimms0110@gmail.com>
2018-11-08 14:29:48 +0100

    Functional mapped keys added

--- src/App.js
+++ src/App.js
@@ -1,28 +1,89 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      error: null,
+      note: null,
+      song: [],
+      isPlaying: false
+    };
+
+    this._handleClick = this._handleClick.bind(this);
+    this._playNote = this._playNote.bind(this);
+    this._endNote = this._endNote.bind(this);
+  }
+
+  componentDidMount() {
+    console.log("Thank you for your interest in my code!");
+  }
   render() {
+    //
+    const keys = ["A", "B", "C", "D", "E", "F", "G"];
+    const mappedKeys = keys.map((el, index) => {
+      return (
+        <button
+          key={index}
+          className="piano-key"
+          //  onClick={() =>this._handleClick()}
+          onMouseDown={() => this._playNote(el)}
+          onMouseUp={() => this._endNote(el)}
+        >
+          {el}
+        </button>
+      );
+    });
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <button
+          className="testbtn"
+          //  onClick={() =>this._handleClick()}
+          onMouseDown={() => this._handleClick("down")}
+          onMouseUp={() => this._handleClick("up")}
+        >
+          TestButton
+        </button>
+        <h2>Mapped Keys</h2>
+        {mappedKeys}
+        <h3>Note Played</h3>
+        {this.state.note}
+        <h3>Your Song</h3>
+        {this.state.song}
       </div>
     );
+  }
+// TEST BUTTON ONLY
+  _handleClick(key, note) {
+    if (key === `down`) this.setState({ note });
+    if (key === `up`) this.setState({ note: null });
+  }
+
+  _playNote(note) {
+    this.setState({ note });
+  }
+
+  _endNote(note) {
+    this.setState({ note: null });
   }
 }
 
 export default App;
+
+// var timer = 0,
+//     timerInterval,
+//     button = document.getElementById("button");
+
+// button.addEventListener("mousedown", function() {
+//   timerInterval = setInterval(function(){
+//     timer += 1;
+//     document.getElementById("timer").innerText = timer;
+//   }, 1000);
+// });
+
+// button.addEventListener("mouseup", function() {
+//   clearInterval(timerInterval);
+//   timer = 0;
+// });
